24e2cdb2a12923f70b8a21e0989c3bfb1a6f0519
Craig Condon <craig@spiceapps.com>
2011-12-04 19:30:08 -0600

    added server.js

---
+++ bin/server.js
@@ -0,0 +1,16 @@
+var args = process.argv.concat(),
+ops = { host: 'localhost' };
+
+
+while(args.length)
+{
+	switch(args.shift())
+	{
+		case '--host':
+		case '-h':
+			ops.host = args.shift();
+		break;
+	}
+}
+
+require('../lib/server').connect(ops);
